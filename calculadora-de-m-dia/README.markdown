f51149d63cbc17aaa247e0d5f5364fa235f6df11
LUCIANO MONTEIRO CAPRARA <77593045+lmonteiroc@users.noreply.github.com>
2021-09-19 15:09:41 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lmonteiroc/pen/KKqvvpK](https://codepen.io/lmonteiroc/pen/KKqvvpK).
+
+
---
+++ calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,17 @@
+function media(){
+     var nota1 = parseFloat(document.getElementById("nota1").value);
+     var nota2 = parseFloat(document.getElementById("nota2").value);
+     var nota3 = parseFloat(document.getElementById("nota3").value);
+     var nota4 = parseFloat(document.getElementById("nota4").value);
+     var media = (nota1 + nota2 + nota3 + nota4)/4 ;
+
+     if(media >= 7)
+      if(media==10)
+       alert("Aprovado!");
+      else
+       alert("Parabens, aprovado! Media "+media);
+     else
+      alert("Reprovado!")
+
+
+}
